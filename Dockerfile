ad1a751d967652dd4cddec76988a6a877816a811
college <raghavbansal054@gmail.com>
2022-12-01 18:26:05 +0530

    second commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:18
 COPY ./out/production/accunknox/ /tmp
+COPY log.txt /tmp
 WORKDIR /tmp
 ENTRYPOINT ["java","function"]
--- src/function.java
+++ src/function.java
@@ -1,23 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class function {
 
-    public static void main(String[] args) {
-        boolean b1 =false;
-        boolean b2=false;
+    public static void main(String[] args) throws Exception {
+        boolean b1 =true;
+        boolean b2=true;
 
 
         ArrayList<Integer> e1 =new ArrayList<Integer>();
         ArrayList<Integer> f1 =new ArrayList<Integer>();
         try {
-            File file=new File("");
+            File file=new File("log.txt");
+            file.createNewFile();
             Scanner sc =new Scanner(file);
             while(sc.hasNextLine())
             {
@@ -25,19 +24,10 @@ public class function {
 
                 e1.add(Integer.parseInt(in[0].replaceAll("[^0-9]","")));
                 f1.add(Integer.parseInt(in[1].replaceAll("[^0-9]","")));
-                if (e1.contains(f1.get(f1.size()-1)))
-                {
-                    if(b1)
-                    {b1=false;}
-                    else
-                    {b2=false;}
-                }
-                if (!b1 && !b2)
-                {
-                    System.out.println("error");
-                    throw new FileNotFoundException();
+                //System.out.println(e1);
+                //System.out.println(f1);
 
-                }
+
 
 
 
@@ -48,12 +38,37 @@ public class function {
         }
         catch (FileNotFoundException e)
         {
-            System.out.println("error");
+            System.out.println("error 2 ");
+            throw new Exception();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (Integer inte: f1) {
+
+
+            if (e1.contains(inte)) {
+                System.out.println("ee");
+                if (b1) {
+                    b1 = false;
+                } else {
+                    b2 = false;
+                    break;
+                }
+            }
 
         }
-        Map<Integer, Long> freq= f1.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
-        freq.entrySet().stream().sorted(Map.Entry.comparingByValue()).limit(3).forEach(System.out::println);
+        if (!b1 && !b2) {
+            System.out.println("error 1 ");
+            throw new Exception();
 
+        }
+        else {
+            Map<Integer, Long> freq = f1.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            System.out.println("Top 3 Menu Items Consumed");
+            freq.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(3).forEach(System.out::println);
+        }
 
     }
 }
